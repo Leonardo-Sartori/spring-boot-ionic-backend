2a3c5053111188c7536dbf8869a9da1c59b937f4
Leonardo Sartori <leonardosartori22@hotmail.com>
2019-06-12 15:29:03 -0300

    Criação da tabela no banco com o JPA

---
+++ src/main/java/com/projeto/backend/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.projeto.backend.repositories;
+
+import com.projeto.backend.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+
+}
---
+++ src/main/java/com/projeto/backend/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.projeto.backend.services;
+
+import com.projeto.backend.domain.Categoria;
+import com.projeto.backend.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria buscar(Integer id){
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+    }
+}
+   
