28f30539e4e11a8e8a3be39f4f953b70cc6d0c70
Leonardo Sartori <leonardosartori22@hotmail.com>
2019-06-12 17:22:32 -0300

    Ajustes no endpoint/categorias/{id}

--- src/main/java/com/projeto/backend/services/CategoriaService.java
+++ src/main/java/com/projeto/backend/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.projeto.backend.services;
 
 import com.projeto.backend.domain.Categoria;
 import com.projeto.backend.repositories.CategoriaRepository;
+import com.projeto.backend.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +14,16 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository repo;
 
-    public Categoria buscar(Integer id){
+    public Categoria find(Integer id) {
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 }
-   
+
+/*
+* public Categoria find(Integer id){
+ Optional<Categoria> obj = repo.findById(id);
+ return obj.orElseThrow(() -> new ObjectNotFoundException(
+"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+}*/
