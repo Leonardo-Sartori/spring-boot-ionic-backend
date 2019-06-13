e2deb8dcc64e7140eb5bedc933cc5664f8e80888
Leonardo Sartori <leonardosartori22@hotmail.com>
2019-06-13 10:57:40 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/projeto/backend/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.projeto.backend.services;
+
+import com.projeto.backend.domain.Cliente;
+import com.projeto.backend.repositories.ClienteRepository;
+import com.projeto.backend.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository repo;
+
+    public Cliente find(Integer id) {
+        Optional<Cliente> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+    }
+}
+
