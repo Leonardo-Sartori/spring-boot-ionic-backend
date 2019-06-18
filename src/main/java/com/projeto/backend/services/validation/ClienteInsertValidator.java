11ecdf3cc0a67b9b60a73246559a5ed29cdf53eb
Leonardo Sartori <leonardosartori22@hotmail.com>
2019-06-18 15:47:16 -0300

    Validacao customizada: email nao repetido na insercao de cliente

---
+++ src/main/java/com/projeto/backend/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,49 @@
+package com.projeto.backend.services.validation;
+
+import com.projeto.backend.domain.Cliente;
+import com.projeto.backend.domain.enums.TipoCliente;
+import com.projeto.backend.dto.ClienteNewDTO;
+import com.projeto.backend.repositories.ClienteRepository;
+import com.projeto.backend.resources.exception.FieldMessage;
+import com.projeto.backend.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteRepository repo;
+
+    @Override
+    public void initialize(ClienteInsert ann) {
+    }
+    @Override
+    public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+        List<FieldMessage> list = new ArrayList<>();
+
+        // inclua os testes aqui, inserindo erros na lista
+
+        if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())){
+            list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+        }
+
+        if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())){
+            list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+        }
+
+        Cliente aux = repo.findByEmail(objDto.getEmail());
+        if(aux != null){
+            list.add(new FieldMessage("email", "Email já existente"));
+        }
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage())
+                    .addPropertyNode(e.getFieldName()).addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
