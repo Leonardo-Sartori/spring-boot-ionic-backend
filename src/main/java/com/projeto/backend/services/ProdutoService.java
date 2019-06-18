11c639cf9770f973320fcc55c8b640d6ce8e2f8a
Leonardo Sartori <leonardosartori22@hotmail.com>
2019-06-18 17:46:55 -0300

    Busca de produtos por nome e categorias

---
+++ src/main/java/com/projeto/backend/services/ProdutoService.java
@@ -0,0 +1,38 @@
+package com.projeto.backend.services;
+
+import com.projeto.backend.domain.Categoria;
+import com.projeto.backend.domain.Pedido;
+import com.projeto.backend.domain.Produto;
+import com.projeto.backend.repositories.CategoriaRepository;
+import com.projeto.backend.repositories.PedidoRepository;
+import com.projeto.backend.repositories.ProdutoRepository;
+import com.projeto.backend.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProdutoService {
+    @Autowired
+    private ProdutoRepository repo;
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public Produto find(Integer id) {
+        Optional<Produto> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+    }
+
+    public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        List<Categoria> categorias = categoriaRepository.findAllById(ids);
+        return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+    }
+}
